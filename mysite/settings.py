efa8b0340cbb16b622b8b2b18ee24a5073e3d86a
whambamthankyoumaam <theirishbonscott@gmail.com>
2017-07-19 16:31:59 -0400

    Second commit: changed database to sqlite3

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangogirls',
-        'USER': 'myblogdbuser',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
